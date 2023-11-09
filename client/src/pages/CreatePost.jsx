1756df509fada74329c8637917eab92e350ff5c9
Thanush19 <110150744+Thanush19@users.noreply.github.com>
2023-11-10 00:39:06 +0530

    cloudimary working

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -2,10 +2,6 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
-// import {
-//   fetchPhotos,
-//   openUploadWidget,
-// } from "../../services/CloudinaryService";
 
 export default function CreatePost() {
   const [title, setTitle] = useState("");
@@ -31,38 +27,6 @@ export default function CreatePost() {
   const handleTagChange = (event) => {
     setTags(event.target.value.split(",").map((tag) => tag.trim()));
   };
-
-  const handleSubmit = async () => {
-    // Prepare the data to be posted to Cloudinary
-    const formData = new FormData();
-    formData.append("file", imageURLs[0]); // Assuming you're posting the first image only
-    formData.append("upload_preset", "blog-thanush"); // Replace with your upload preset
-
-    try {
-      // Post the image to Cloudinary
-      const response = await axios.post(
-        "https://api.cloudinary.com/v1_1/ddxqeeype/image/upload",
-        formData
-      );
-
-      // You can now access the uploaded image URL from the response
-      const uploadedImageURL = response.data.secure_url;
-
-      // Now, you can submit other data, including title, description, content, tags, and the uploaded image URL to your server.
-      const postData = {
-        title,
-        description,
-        content,
-        tags,
-        imageURLs: [uploadedImageURL], // Update with the uploaded image URL
-      };
-
-      // Send `postData` to your server for further processing.
-      console.log("Data to be sent to the server:", postData);
-    } catch (error) {
-      console.error("Error uploading image to Cloudinary:", error);
-    }
-  };
   const cloudinaryRef = useRef();
   const widgetRef = useRef();
   useEffect(() => {
@@ -77,6 +41,7 @@ export default function CreatePost() {
       }
     );
   }, []);
+
   return (
     <div className="CreatePost">
       <h3>Create a New Post</h3>
@@ -108,16 +73,8 @@ export default function CreatePost() {
 
       <ReactQuill theme="snow" value={content} onChange={handleContentChange} />
 
-      {/* <button onClick={() => beginUpload("image")}>Upload Image</button> */}
-      {/* <section>
-        {imageURLs.map((imageUrl, index) => (
-          <img key={index} src={imageUrl} alt={`Image ${index}`} />
-        ))}
-      </section> */}
       <button onClick={() => widgetRef.current.open()}>upload</button>
-      <button type="button" onClick={handleSubmit}>
-        Submit
-      </button>
+      <button type="button">Submit</button>
     </div>
   );
 }
---
+++ client/utils/constants.js
@@ -0,0 +1,5 @@
+// Define the constant
+const backend_url = "http://localhost:3000/api";
+
+// Export the constant
+export default backend_url;
